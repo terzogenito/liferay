1a1ea1b62482850b9deaa0b456520bb0c606b1e1
terzogenito <otineg@gmail.com>
2024-12-10 19:01:34 +0700

    Initial Commit

---
+++ liferay-workspace/modules/spring-liferay-module/src/main/java/com/example/spring/Controller.java
@@ -0,0 +1,14 @@
+package com.example.spring;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello from Liferay Spring Boot Module!";
+    }
+}
+
